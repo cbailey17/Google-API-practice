8de02a9de9a0eda2a1368d3fb8d517d63a409bd3
cbailey17 <cam.mail@comcast.net>
2022-03-29 18:58:14 -0700

    Hello world for Google API using the javascript api

---
+++ typescript.ts
@@ -0,0 +1,16 @@
+// Initialize and add the map
+function initMap(): void {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+      zoom: 4,
+      center: San Diego,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: San Diego,
+      map: map,
+    });
+  }
+  
